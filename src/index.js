821b3a8bbacd245da4f48df1bf5c4aa83c3b52c8
aditya8948 <89adityapandey@gmail.com>
2026-06-16 01:27:13 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const {serverConfig, Logger } = require("./config");
+const app = express();
+
+const apiRoutes = require('./routes');
+
+app.use('/api' , apiRoutes)
+
+
+app.listen(serverConfig.PORT , () => {
+    console.log(`Server is running on port ${serverConfig.PORT}`);
+    Logger.info("succesfully started the server ", "root", {})
+}); 
+ 
